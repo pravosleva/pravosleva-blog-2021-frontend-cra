296bd01fa4832c0fd853943147c54411bc76c490
Den <selection4test@gmail.com>
2021-08-24 12:05:54 +0300

    postbuild script added

---
+++ postbuild.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+EXTERNAL_DIR=""$(dirname "$PWD")""
+
+# See also https://stackoverflow.com/questions/41495658/use-custom-build-output-folder-when-using-create-react-app
+cd build &&
+
+if [ ! -d "${EXTERNAL_DIR}/backend/public/cra" ]
+  then
+    echo "${EXTERNAL_DIR}/backend/public/cra Should be created"
+    mkdir "${EXTERNAL_DIR}/backend/public/cra"
+fi
+
+echo '1/3 Clear "backend/public/cra/*" directory...'
+rm -rf ${EXTERNAL_DIR}/backend/public/demo/*
+
+echo '2/3 Move fersh build...'
+mv -v ${EXTERNAL_DIR}/frontend/build/* "${EXTERNAL_DIR}/backend/public/cra/"
+
+echo '3/3 Remove frontend/build directory.'
+rm -rf ./build
+# Done

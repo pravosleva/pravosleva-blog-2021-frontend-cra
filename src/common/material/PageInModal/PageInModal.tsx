3b6d39f0ba55a2ce57aa9c1b5235b37fe100795b
Den <selection4test@gmail.com>
2022-06-17 03:26:36 +0300

    ui updated

--- src/common/material/PageInModal/PageInModal.tsx
+++ src/common/material/PageInModal/PageInModal.tsx
@@ -6,9 +6,11 @@ import { Modal } from '~/common/material/Modal'
 import { mdiLoading } from '@mdi/js';
 import Icon from '@mdi/react'
 import { Content } from './components'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
 import Alert from '@material-ui/lab/Alert'
+import styles from './PageInModal.module.scss'
+import { getNormalizedDate } from '~/utils/timeConverter'
 
 export const PageInModal = () => {
   const dispatch = useDispatch()
@@ -24,6 +26,11 @@ export const PageInModal = () => {
   const history = useHistory()
 
   useEffect(() => {
+  console.log(projectData)
+
+  }, [projectData])
+
+  useEffect(() => {
     const queryParams = new URLSearchParams(location.search)
 
     if(!!queryParams.has('open')) {
@@ -37,6 +44,13 @@ export const PageInModal = () => {
       })
     }
   }, [dispatch, location.search, history])
+  const bgImg = useMemo(() => {
+    return projectData?.metadata.shareImage?.formats?.large
+      || projectData?.metadata.shareImage?.formats?.medium
+      || projectData?.metadata.shareImage?.formats?.small
+      || projectData?.metadata.shareImage?.formats?.thumbnail
+      || null
+  }, [projectData])
 
   return (
     <Modal
@@ -48,21 +62,60 @@ export const PageInModal = () => {
       maxWidth={!!errMsg || isProjectLoading ? 'sm' : 'md'}
       fullWidth
       contentRenderer={() => (
-        <div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
           {
-            isProjectLoading ? (
-              <div style={{ display: 'flex', justifyContent: 'center', color: 'lightgray' }}>
-                <Icon size={4} path={mdiLoading} spin={1}/>
+            !!bgImg && (
+              <div
+                className={styles['img-preview-wrapper']}
+                style={{
+                  minHeight: '100px',
+                  backgroundImage: `url(${bgImg.url})`,
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                  marginBottom: '8px',
+                }}
+              >
+                <div
+                  className={styles['img-preview-container']}
+                  style={{
+                    background: 'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.7))',
+                    height: '100%',
+
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <h1 className={styles['title']} style={{ marginBottom: '24px' }}>{isProjectLoaded ? projectData?.metadata.metaDescription : (isProjectLoading ? 'Please wait...' : 'Sorry')}</h1>
+                  <div className={styles['created-updated-box']}>
+                    <div>Created {getNormalizedDate(projectData.createdAt)}</div>
+                    <div>Updated {getNormalizedDate(projectData.updatedAt)}</div>
+                  </div>
+                </div>
               </div>
-            ) :
-              isProjectLoaded ? (
-                <Content sections={projectData.contentSections} />
-              ) : (
-                <Alert variant="outlined" severity="error" title="Oops...">
-                  {errMsg || 'Что-то пошло не так'}
-                </Alert>
-              )
+            )
           }
+          <div>
+            {
+              isProjectLoading ? (
+                <div style={{ display: 'flex', justifyContent: 'center', color: 'lightgray' }}>
+                  <Icon size={4} path={mdiLoading} spin={1}/>
+                </div>
+              ) :
+                isProjectLoaded ? (
+                  <Content sections={projectData.contentSections} />
+                ) : (
+                  <Alert variant="outlined" severity="error" title="Oops...">
+                    {errMsg || 'Что-то пошло не так'}
+                  </Alert>
+                )
+            }
+          </div>
         </div>
       )}
       actionsRenderer={() => (
